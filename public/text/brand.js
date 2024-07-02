3646f36dd729c21e4668fc9eb9d10b48ed6d9220
Touseef-EL <touseef@exertlogics.com>
2024-07-02 16:38:42 +0500

    add favicon

--- public/text/brand.js
+++ public/text/brand.js
@@ -1,14 +1,14 @@
 module.exports = {
   agency: {
-    name: 'ExertLogics',
-    desc: 'ExertLogics - React Single Landing Page Template',
-    prefix: 'exertlogics',
-    footerText: 'ExertLogics - All Rights Reserved 2024',
-    logoText: 'ExertLogics',
-    projectName: 'ExertLogics',
-    url: 'luxireact.ux-maestro.com/agency',
-    img: '/public/images/el-logo.png',
+    name: "ExertLogics",
+    desc: "ExertLogics",
+    prefix: "exertlogics",
+    footerText: "ExertLogics - All Rights Reserved 2024",
+    logoText: "ExertLogics",
+    projectName: "ExertLogics",
+    url: "https://exertlogics.vercel.app/",
+    img: "/public/images/el-logo.png",
     notifMsg:
-      'We are running a software agency! Experience seamless solutions and unparalleled service. Contact us to elevate your business today.',
+      "We are running a software agency! Experience seamless solutions and unparalleled service. Contact us to elevate your business today.",
   },
 };
